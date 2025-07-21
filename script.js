43809f2dc0dc4fc6089ebb9442aa52a4889dd06f
Tamim Hosen <onlyforunlisted1@gmail.com>
2025-07-21 17:44:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const passwordInput = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", function () {
+  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+  this.textContent = type === "password" ? "👁️" : "🙈";
+});
+
+function checkPassword() {
+  const userInput = passwordInput.value;
+
+  const passwords = {
+    "174894": "https://www.399bet11.com/?r=gob5502",
+    "654321": "https://www.google.com",
+    "000000": "https://m.youtube.com",
+    "707070": "https://m.facebook.com"
+  };
+
+  if (passwords[userInput]) {
+    window.location.href = passwords[userInput];
+  } else {
+    document.getElementById("message").innerText = "🚫 Incorrect Password!";
+  }
+}
